e2472ed422cda3292faf76096eefa8b7bec36607
Cole <49996960+CWoulf@users.noreply.github.com>
2020-02-11 11:03:16 -0600

    Shield Bash with big read

---
+++ Assets/Scripts/ShieldBash.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldBash : MonoBehaviour
+{
+
+    //public Animator animator;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+
+    public LayerMask enemyLayers;
+
+    public int attackDamage = 100;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       if(Input.GetKey(KeyCode.Space))
+        {
+         
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        Debug.Log("D1");
+        //Play attack animation
+        //animator.SetTrigger("Attack");
+
+        //Detect enemies in rangle of attack
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        Debug.Log("D2");
+        //Damage Them
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit " + enemy.name);
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+    }
+    
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+    
+}
+
