eebd6bd6301a738447a6ade9c059be12848ebe58
C <C0d3st3r@users.noreply.github.com>
2020-02-14 10:34:32 -0600

    Dialogue Box fully working
    Player freezes while texbox is open

---
+++ Assets/Scripts/DialogueCollider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class DialogueCollider : MonoBehaviour
+{
+  public DialogueTrigger trigger;
+    public void OnCollisionEnter2D(Collision2D thing)
+  {
+    trigger.startConversation();
+    Debug.Log("collision detected");
+  }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class PlayerMovement : MonoBehaviour
 
 {
     public float speed = 0f;
-
+    public Animator textBoxAnimator;
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (textBoxAnimator.GetBool("isOpen")){
+            return;
+
+        }
         if (Input.GetKey(KeyCode.W)) {
             transform.Translate(new Vector3(0f, speed * Time.deltaTime,0f));
 
