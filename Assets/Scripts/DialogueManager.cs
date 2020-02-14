4f9a166e99215bab18d5bba3b02ec8824c4f53d1
C <C0d3st3r@users.noreply.github.com>
2020-02-13 21:04:30 -0600

    Rough Dialogue Box
    Goes in and out and goes through lines of dialogue

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+  public Text nameText;
+  public Text lineText;
+  public Animator textBoxAnimator;
+  private Queue<string> dialogueLines;
+  
+  // Start is called before the first frame update
+  void Start()
+    {
+    dialogueLines = new Queue<string>();
+    }
+
+  public void startDialogue(Dialogue d)
+  {
+    textBoxAnimator.SetBool("isOpen", true);
+    nameText.text = d.name;
+
+    dialogueLines.Clear();
+    foreach (string line in d.dialogueLines)
+    {
+      dialogueLines.Enqueue(line);
+    }
+
+    displayNextLine();
+  }
+
+  public void displayNextLine()
+  {
+    if (dialogueLines.Count == 0)
+    {
+      endConversation();
+      return;
+    }
+
+    string line = dialogueLines.Dequeue();
+    lineText.text = line;
+  }
+
+  public void endConversation()
+  {
+    textBoxAnimator.SetBool("isOpen", false);
+  }
+}
