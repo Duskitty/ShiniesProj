899f015b1252336d8a2654c3ba390e0874bdd140
Beodrich <60353906+Beodrich@users.noreply.github.com>
2020-02-17 17:48:21 -0600

    commited out the light code

--- Assets/Scripts/light.cs
+++ Assets/Scripts/light.cs
@@ -20,8 +20,8 @@ public class light : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RayCastHit2D hit = Physics2D.Raycast(transform.position, transform.up);
-        Debug.DrawLine(transform.position, attackpoint);
+        //RayCastHit2D hit = Physics2D.Raycast(transform.position, transform.up);
+        //Debug.DrawLine(transform.position, attackpoint);
 
     }
 }
