7353cd75d805672bbad285652158f7aada12bf94
Cole <49996960+CWoulf@users.noreply.github.com>
2020-02-17 14:57:30 -0600

    Raycasting
    Cant get it to work on my computer so gonna try working on it more tomorrow

---
+++ Assets/Scripts/light.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class light : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    public Transform lightHit;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer =GetComponent<LineRenderer>();
+        lineRenderer.enabled = false;
+        lineRenderer.useWorldSpace = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RayCastHit2D hit = Physics2D.Raycast(transform.position, transform.up);
+        Debug.DrawLine(transform.position, attackpoint);
+
+    }
+}
