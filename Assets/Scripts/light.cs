dda0015b72142b641733c6e0483c4e4e32aa952a
Cole <49996960+CWoulf@users.noreply.github.com>
2020-02-18 08:53:11 -0600

    Update light.cs

--- Assets/Scripts/light.cs
+++ Assets/Scripts/light.cs
@@ -20,13 +20,18 @@ public class Light : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
-        //RayCastHit2D hit = Physics2D.Raycast(transform.position, transform.up);
-        //Debug.DrawLine(transform.position, attackpoint);
-=======
         RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up);
         Debug.DrawLine(transform.position, hit.point);
->>>>>>> 14e0c4edc7ff5b1fa93dd4104aa51eb6d5341d55
-
+        lightHit.position = hit.point;
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, lightHit.position);
+        if(Input.GetKey(KeyCode.Q))
+        {
+            lineRenderer.enabled = true;
+        }
+        else
+        {
+            lineRenderer.enabled = false;
+        }
     }
 }
