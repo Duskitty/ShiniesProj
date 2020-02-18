2cafc38ee6ba8d5b0069a58bfe5fc9fd7b9b0a50
C <C0d3st3r@users.noreply.github.com>
2020-02-18 08:49:26 -0600

    Made a separate scene to test the text box

--- Assets/Scripts/light.cs
+++ Assets/Scripts/light.cs
@@ -20,13 +20,13 @@ public class Light : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
+//<<<<<<< HEAD
         //RayCastHit2D hit = Physics2D.Raycast(transform.position, transform.up);
         //Debug.DrawLine(transform.position, attackpoint);
-=======
+//=======
         RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up);
         Debug.DrawLine(transform.position, hit.point);
->>>>>>> 14e0c4edc7ff5b1fa93dd4104aa51eb6d5341d55
+//>>>>>>> 14e0c4edc7ff5b1fa93dd4104aa51eb6d5341d55
 
     }
 }
