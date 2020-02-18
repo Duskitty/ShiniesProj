4fb3c258ae0fadcc6a32338723667aa5173962a5
Cole <49996960+CWoulf@users.noreply.github.com>
2020-02-18 10:23:55 -0600

    Please Work
    pppppllllleeeaase

--- Assets/Scripts/SunBeam.cs
+++ Assets/Scripts/SunBeam.cs
@@ -2,11 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Light : MonoBehaviour
+public class SunBeam : MonoBehaviour
 {
     private LineRenderer lineRenderer;
-    public Transform lightHits;
-
+    public Transform lightHit;
 
     // Start is called before the first frame update
     void Start()
@@ -14,22 +13,21 @@ public class Light : MonoBehaviour
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.enabled = true;
         lineRenderer.useWorldSpace = true;
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(0.0f,-1.0f));
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(0.0f, -1.0f));
         Debug.DrawLine(transform.position, hit.point);
-        lightHits.position = hit.point;
+        lightHit.position = hit.point;
         lineRenderer.SetPosition(0, transform.position);
-        lineRenderer.SetPosition(1, lightHits.position);
-        if(hit.collider != null)
+        lineRenderer.SetPosition(1, lightHit.position);
+        if (hit.collider != null)
         {
             print(hit.collider.name);
         }
-        
+
         /*
         if()
         {
