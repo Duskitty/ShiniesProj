b5b182ec521fd694f6708888d9ff5cddbba3b42f
Beodrich <60353906+Beodrich@users.noreply.github.com>
2020-02-20 11:04:25 -0600

    added animations
    work in progress

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 [System.Serializable]
 public class PlayerMovement : MonoBehaviour
-
+    //1 is up, 2 is down, 3 is left and 4 is right
 {
-   
+    private float horizontalMove = 0f;
+    private float verticalMove = 0f;
+    public Animator animator;
     public float speed = 0f;
     public Animator textBoxAnimator;
     public Rigidbody2D player;
     // Update is called once per frame
     void FixedUpdate()
     {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        //animator.SetFloat("Speed", Mathf.Abs(verticalMove));
         if (textBoxAnimator.GetBool("isOpen")){
             return;
 
@@ -20,23 +25,27 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.W)) {
             transform.Translate(new Vector3(0f, speed * Time.deltaTime,0f));
 
-
         }
         if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(new Vector3(0f, -speed * Time.deltaTime,0f));
 
-
         }
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(new Vector3(-speed*Time.deltaTime,0f,0f));
+            animator.SetBool("isLeft", true);
+            if (horizontalMove < 0.01) {
+                animator.SetBool("isLeft", true);
 
-            
+
+            }
+
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(new Vector3(speed*Time.deltaTime,0f,0f));
+            animator.SetBool("isRight", true);
 
 
         }
