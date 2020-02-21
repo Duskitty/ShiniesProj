3fb160213196f3967bf366b64aa06c18682606be
C <C0d3st3r@users.noreply.github.com>
2020-02-20 21:28:13 -0600

    Light renderer and reflection

--- Assets/Scripts/SunBeam.cs
+++ Assets/Scripts/SunBeam.cs
@@ -6,45 +6,51 @@ public class SunBeam : MonoBehaviour
 {
     private LineRenderer lineRenderer;
     public Transform lightHit;
+    public Transform beam1;
+    public Transform beam2;
+    public Transform beam3;
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.enabled = true;
         lineRenderer.useWorldSpace = true;
-    }
+        beam1.GetComponent<Renderer>().enabled = false;
+        beam2.GetComponent<Renderer>().enabled = false;
+        beam3.GetComponent<Renderer>().enabled = false;
+  }
 
-    // Update is called once per frame
-    void Update()
+  // Update is called once per frame
+  void Update()
+  {
+    RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector3.up));
+    if(hit.collider != null)
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(0.0f, -1.0f));
-        Debug.DrawLine(transform.position, hit.point);
-        lightHit.position = hit.point;
-        lineRenderer.SetPosition(0, transform.position);
-        lineRenderer.SetPosition(1, lightHit.position);
-        if (hit.collider != null)
-        {
-            //print(hit.collider.name);
-        }
+      Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.up));
+      lightHit.position = hit.point;
+      lineRenderer.SetPosition(0, transform.position);
+      lineRenderer.SetPosition(1, lightHit.position);
+      lineRenderer.enabled = true;
 
-        /*
-        if(Collider2D.)
-        {
-            lineRenderer.enabled = true;
-        }
-        else
-        {
-            lineRenderer.enabled = false;
-        }
-        */
+      if(hit.collider.name == "Gold Man")
+      {
+        beam1.GetComponent<Renderer>().enabled = true;
+        beam2.GetComponent<Renderer>().enabled = true;
+        beam3.GetComponent<Renderer>().enabled = true;
+      }
+      else
+      {
         
+      }
     }
-    void OnCollisionEnter2D(Collision2D col)
+
+    else
     {
-
-        //Stuff that happens when the collider collides with something
-        lineRenderer.enabled = true;
-
+      lineRenderer.enabled = false;
+      beam1.GetComponent<Renderer>().enabled = false;
+      beam2.GetComponent<Renderer>().enabled = false;
+      beam3.GetComponent<Renderer>().enabled = false;
     }
+  }
 }
