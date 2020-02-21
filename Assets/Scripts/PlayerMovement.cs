421b3713faa73f6f4a7b818c9480666e17776b2a
Beodrich <60353906+Beodrich@users.noreply.github.com>
2020-02-21 16:10:26 -0600

    added animatons

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -25,6 +25,10 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(new Vector3(0f, speed * Time.deltaTime, 0f));
+            animator.SetBool("isUp", true);
+            animator.SetBool("isDown", false);
+            animator.SetBool("isLeft", false);
+            animator.SetBool("isRight", false);
 
         }
         if (Input.GetKey(KeyCode.S))
@@ -33,16 +37,17 @@ public class PlayerMovement : MonoBehaviour
             animator.SetBool("isDown", true);
             animator.SetBool("isRight", false);
             animator.SetBool("isLeft", false);
-           
+            animator.SetBool("isUp", false);
+
         }
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(new Vector3(-speed * Time.deltaTime, 0f, 0f));
             
-                animator.SetBool("isLeft", true);
-                animator.SetBool("isRight", false);
-                 animator.SetBool("isDown", false);
-
+             animator.SetBool("isLeft", true);
+            animator.SetBool("isRight", false);
+            animator.SetBool("isDown", false);
+            animator.SetBool("isUp", false);
 
 
 
@@ -56,16 +61,17 @@ public class PlayerMovement : MonoBehaviour
                 animator.SetBool("isRight", true);
                 animator.SetBool("isLeft", false);
                  animator.SetBool("isDown", false);
+            animator.SetBool("isUp", false);
 
 
 
 
         }
-        if (player.velocity.y == 0)
-        {
-            animator.SetFloat("Speed", -1);
-            animator.SetBool("isDown", false);
+        /*  if (player.velocity.y == 0)
+          {
+              animator.SetFloat("Speed", -1);
+              animator.SetBool("isDown", false);
 
-        }
+          }*/
     }
 }
