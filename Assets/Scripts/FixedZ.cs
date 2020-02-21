9a57db5d6ae94a3fa5c3de4e6883a3f86b3f483e
Cole <49996960+CWoulf@users.noreply.github.com>
2020-02-21 15:12:08 -0600

    Z Rotation
    no more rotating when hitting colliders

---
+++ Assets/Scripts/FixedZ.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedZ : MonoBehaviour
+{
+    float lockPos = 0;
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(lockPos, lockPos, lockPos);
+    }
+}
