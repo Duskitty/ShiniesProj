b59e94ccb1bbb800f23a37a760627d27c52eabf0
Cole <49996960+CWoulf@users.noreply.github.com>
2020-03-05 14:11:39 -0600

    trying to fix things

---
+++ Assets/newMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newMovement : MonoBehaviour
+{
+    private float horizontalMovment = 0f;
+    private float verticalMovment = 0f;
+    public Animator anim;
+    //animations script for mushroom man
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (horizontalMovment > 0)
+        {
+            anim.SetBool("Right", true);
+            anim.SetBool("Moving", true);
+            anim.SetBool("Left", false);
+            anim.SetBool("Up", false);
+            anim.SetBool("Down", false);
+
+
+
+        }
+        if (horizontalMovment < 0)
+        {
+            anim.SetBool("Left", true);
+            anim.SetBool("Moving", true);
+            anim.SetBool("Right", false);
+            anim.SetBool("Up", false);
+            anim.SetBool("Down", false);
+
+
+        }
+        if (verticalMovment > 0)
+        {
+            anim.SetBool("Left", false);
+            anim.SetBool("Moving", true);
+            anim.SetBool("Right", false);
+            anim.SetBool("Up", true);
+            anim.SetBool("Down", false);
+
+        }
+        if (verticalMovment < 0)
+        {
+            anim.SetBool("Left", false);
+            anim.SetBool("Moving", true);
+            anim.SetBool("Right", false);
+            anim.SetBool("Up", false);
+            anim.SetBool("Down", true);
+
+        }
+    }
+}
