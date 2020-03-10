f337e79147a6b8fe36ebfd6cb23c7dfb1389d8dd
Beodrich <60353906+Beodrich@users.noreply.github.com>
2020-03-10 10:40:11 -0500

    jgfdrsvfjkjv.3.0

---
+++ Assets/Scripts/RockSmash.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockSmash : MonoBehaviour
+{
+    private bool isBashing = false;
+    public float rockDelay;
+    public Animator animi;
+
+
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        if (Input.GetKey(KeyCode.Space) && Input.GetKey(KeyCode.LeftShift)) { 
+            
+                animi.SetBool("Break", true);
+                StartCoroutine(RockDie());
+            
+
+        }
+    }
+
+
+
+    public void SetIsBashing(bool bashing) {
+        Debug.Log("made it to the setter");
+        isBashing = bashing;
+        Debug.Log(isBashing);
+    }
+
+    public IEnumerator RockDie() {
+        yield return new WaitForSeconds(rockDelay);
+        Destroy(gameObject);
+        animi.SetBool("Break", false);
+
+    }
+}
