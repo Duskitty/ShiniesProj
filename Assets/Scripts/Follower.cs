28aa9a57aa4668efece8c5d25a62ff7cd5c424cc
Duskitty <Theduskitty@gmail.com>
2020-03-10 11:03:58 -0500

    AddedBugboi

---
+++ Assets/Scripts/Follower.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+
+    [SerializeField]private GameObject Sel;
+    [SerializeField] private Vector2 Min;
+    [SerializeField] private Vector2 Max;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+    transform.position = new Vector3
+    (
+    Mathf.Clamp(Sel.transform.position.x, Min.x, Max.x),
+    Mathf.Clamp(Sel.transform.position.y, Min.y, Max.y),
+    transform.position.z
+    );
+    }
+}
