0a9200cf29d50ac7d12f2309beb65cfb2dee040d
Cole <49996960+CWoulf@users.noreply.github.com>
2020-04-02 10:13:04 -0500

    asfd

--- Assets/SwapGem.cs
+++ Assets/SwapGem.cs
@@ -4,29 +4,15 @@ using UnityEngine;
 
 public class SwapGem : MonoBehaviour
 {
-    function Update()
+    // Start is called before the first frame update
+    void Start()
     {
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, hit, 100))
-        {
-            if (!noObj)//no object picked yet
-            {
-                noObj = hit.transform;//save picked objects transform
-                tempObj = noObj.transform.position;
-            }
-            else if (noObj != null)
-            { //if noObject now has a transform
-                switchObj = hit.transform;
-                DoTheSwitch();
-            }
-        }
+        
     }
 
-    function DoTheSwitch()
+    // Update is called once per frame
+    void Update()
     {
-        noObj.transform.position = switchObj.transform.position;//moves the first clicked object to the second clicke objects position
-        switchObj.transform.position = tempObj;
-        noObj = null;
+        
     }
 }
