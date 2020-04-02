ff4364779f42acb6714d03de15310bbff8e85912
Beodrich <60353906+Beodrich@users.noreply.github.com>
2020-04-02 11:02:12 -0500

    ui stuff

---
+++ Assets/Scripts/GemPick.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemPick : MonoBehaviour
+{
+    public static bool reflectGem = false;
+    public static bool fireGem = false;
+    public static bool iceGem = false;
+    //add other gems later 
+
+
+
+   public void FireGem() {
+        fireGem = true;
+        reflectGem = false;
+        iceGem = false;
+    
+    }
+  public  void ReflectGem() {
+
+        fireGem = false;
+        reflectGem = true;
+        iceGem = false;
+    }
+   public void IceGem() {
+        fireGem = false;
+        reflectGem = false;
+        iceGem = true;
+
+    }
+}
