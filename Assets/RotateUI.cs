b4435b5aab837587c26db454f8182684960e6c19
Cole <49996960+CWoulf@users.noreply.github.com>
2020-04-02 10:11:51 -0500

    ui stuff

---
+++ Assets/RotateUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateUI : MonoBehaviour
+{
+    public GameObject other;
+
+    void Swap()
+    {
+        Vector3 temp = transform.position;
+        transform.position = other.transform.position;
+        other.transform.position = temp;
+    }
+}
---
+++ Assets/SwapGem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapGem : MonoBehaviour
+{
+    function Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, hit, 100))
+        {
+            if (!noObj)//no object picked yet
+            {
+                noObj = hit.transform;//save picked objects transform
+                tempObj = noObj.transform.position;
+            }
+            else if (noObj != null)
+            { //if noObject now has a transform
+                switchObj = hit.transform;
+                DoTheSwitch();
+            }
+        }
+    }
+
+    function DoTheSwitch()
+    {
+        noObj.transform.position = switchObj.transform.position;//moves the first clicked object to the second clicke objects position
+        switchObj.transform.position = tempObj;
+        noObj = null;
+    }
+}
