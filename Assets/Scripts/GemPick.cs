b5b72a2d3956d32206eb1c9a989abd49abea2ea0
Beodrich <60353906+Beodrich@users.noreply.github.com>
2020-04-03 13:10:13 -0500

    stuff

--- Assets/Scripts/GemPick.cs
+++ Assets/Scripts/GemPick.cs
@@ -8,25 +8,31 @@ public class GemPick : MonoBehaviour
     public static bool fireGem = false;
     public static bool iceGem = false;
     //add other gems later 
-
+    private Color fireColor;
+    private Color reflectColor;
+    private Color iceColor;
 
 
    public void FireGem() {
         fireGem = true;
         reflectGem = false;
         iceGem = false;
-    
+        Debug.Log("Fire gem selected");
     }
   public  void ReflectGem() {
 
         fireGem = false;
         reflectGem = true;
         iceGem = false;
+        Debug.Log("Reflect gem selected");
+
     }
-   public void IceGem() {
+    public void IceGem() {
         fireGem = false;
         reflectGem = false;
         iceGem = true;
+        Debug.Log("Ice gem selected");
+
 
     }
 }
