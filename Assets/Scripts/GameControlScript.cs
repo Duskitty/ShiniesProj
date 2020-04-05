2ed59a4bbd4d5a8798924f998fe6ce66a23844f9
Beodrich <60353906+Beodrich@users.noreply.github.com>
2020-04-05 14:18:22 -0500

    j

--- Assets/Scripts/GameControlScript.cs
+++ Assets/Scripts/GameControlScript.cs
@@ -45,29 +45,29 @@ public class GameControlScript : MonoBehaviour
         }
         else if (sceneName == "World_2")
         {
-            
-            if (charges > 1)
-            charges = 1;
 
-        switch (charges)
-        {
-            case 0:
-                chargedGem1.gameObject.SetActive(false);
-                chargedGem2.gameObject.SetActive(false);
-                chargedGem3.gameObject.SetActive(false);
-                unchargedGem1.gameObject.SetActive(true);
-                unchargedGem2.gameObject.SetActive(false);
-                unchargedGem3.gameObject.SetActive(false);
-                break;
-            case 1:
-                chargedGem1.gameObject.SetActive(true);
-                chargedGem2.gameObject.SetActive(false);
-                chargedGem3.gameObject.SetActive(false);
-                unchargedGem1.gameObject.SetActive(false);
-                unchargedGem2.gameObject.SetActive(false);
-                unchargedGem3.gameObject.SetActive(false);
-                break;
-        }
+            if (charges > 1)
+                charges = 1;
+
+            switch (charges)
+            {
+                case 0:
+                    chargedGem1.gameObject.SetActive(false);
+                    chargedGem2.gameObject.SetActive(false);
+                    chargedGem3.gameObject.SetActive(false);
+                    unchargedGem1.gameObject.SetActive(true);
+                    unchargedGem2.gameObject.SetActive(false);
+                    unchargedGem3.gameObject.SetActive(false);
+                    break;
+                case 1:
+                    chargedGem1.gameObject.SetActive(true);
+                    chargedGem2.gameObject.SetActive(false);
+                    chargedGem3.gameObject.SetActive(false);
+                    unchargedGem1.gameObject.SetActive(false);
+                    unchargedGem2.gameObject.SetActive(false);
+                    unchargedGem3.gameObject.SetActive(false);
+                    break;
+            }
             /*
         }
         else if (sceneName == "World_3")
@@ -142,39 +142,40 @@ public class GameControlScript : MonoBehaviour
         }
         */
 
-        // gameOver.gameObject.SetActive(false);
-    }
+            // gameOver.gameObject.SetActive(false);
+        }
 
-    void Update()
-    {
-        if (health > 3)
-            health = 3;
-
-        switch (health)
+        void Update()
         {
-            case 3:
-                heart1.gameObject.SetActive(true);
-                heart2.gameObject.SetActive(true);
-                heart3.gameObject.SetActive(true);
-                break;
-            case 2:
-                heart1.gameObject.SetActive(true);
-                heart2.gameObject.SetActive(true);
-                heart3.gameObject.SetActive(false);
-                break;
-            case 1:
-                heart1.gameObject.SetActive(true);
-                heart2.gameObject.SetActive(false);
-                heart3.gameObject.SetActive(false);
-                break;
-            case 0:
-                heart1.gameObject.SetActive(false);
-                heart2.gameObject.SetActive(false);
-                heart3.gameObject.SetActive(false);
-               // gameOver.gameObject.SetActive(true);
-                Time.timeScale = 0; // makes game stop when all 3 lives are lost
-                break;
+            if (health > 3)
+                health = 3;
 
+            switch (health)
+            {
+                case 3:
+                    heart1.gameObject.SetActive(true);
+                    heart2.gameObject.SetActive(true);
+                    heart3.gameObject.SetActive(true);
+                    break;
+                case 2:
+                    heart1.gameObject.SetActive(true);
+                    heart2.gameObject.SetActive(true);
+                    heart3.gameObject.SetActive(false);
+                    break;
+                case 1:
+                    heart1.gameObject.SetActive(true);
+                    heart2.gameObject.SetActive(false);
+                    heart3.gameObject.SetActive(false);
+                    break;
+                case 0:
+                    heart1.gameObject.SetActive(false);
+                    heart2.gameObject.SetActive(false);
+                    heart3.gameObject.SetActive(false);
+                    // gameOver.gameObject.SetActive(true);
+                    Time.timeScale = 0; // makes game stop when all 3 lives are lost
+                    break;
+
+            }
         }
     }
 }
