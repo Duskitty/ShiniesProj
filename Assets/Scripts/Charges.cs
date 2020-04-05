546f104741752fc9e670150d0736c973978dbc15
Beodrich <60353906+Beodrich@users.noreply.github.com>
2020-04-05 14:53:01 -0500

    vxvdx

--- Assets/Scripts/Charges.cs
+++ Assets/Scripts/Charges.cs
@@ -9,11 +9,25 @@ public class Charges : MonoBehaviour
         if (Input.GetKey(KeyCode.C)) {
 
             GameControlScript.charges -= 1;//decrmate the charges
+
             if (GameControlScript.charges < 0) {
                 GameControlScript.charges = 0;//make sure charges dont go below zero
             
             }
         
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag.Equals("sunbeam"))
+        {
+            GameControlScript.charges += 1;
+            if (GameControlScript.charges > 1)
+            {
+                GameControlScript.charges = 1;//keep for world one get ride for other worlds :)
+
+            }
+
         }
     }
 }
