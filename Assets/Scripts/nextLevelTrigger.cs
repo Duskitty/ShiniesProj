b394e2da7525592c7aac75e17a88a0e3e8ba5855
Beodrich <60353906+Beodrich@users.noreply.github.com>
2020-04-07 11:02:46 -0500

    stuff

--- Assets/Scripts/nextLevelTrigger.cs
+++ Assets/Scripts/nextLevelTrigger.cs
@@ -5,11 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class nextLevelTrigger : MonoBehaviour
 {
-   
+    Scene currentScence;
   public void OnTriggerEnter2D(Collider2D thing)
   {
-        Debug.Log("entered next level trigger");
-        SceneManager.LoadScene("World_1_Boss", LoadSceneMode.Single);
-        
-  }
+        if (currentScence.name == "World_1")
+        {
+            Debug.Log("entered next level trigger");
+            SceneManager.LoadScene("World_1_Boss", LoadSceneMode.Single);
+        }
+       else if (currentScence.name == "World_1_Boss")
+        {
+           // Debug.Log("entered next level trigger");
+            SceneManager.LoadScene("World 2 P1", LoadSceneMode.Single);
+        }
+        else if (currentScence.name == "World 2 P2")
+        {
+            // Debug.Log("entered next level trigger");
+            SceneManager.LoadScene("YouWin", LoadSceneMode.Single);
+        }
+
+    }
+
+    private void Update()
+    {
+         currentScence = SceneManager.GetActiveScene();
+        Debug.Log(currentScence.name);
+    }
 }
