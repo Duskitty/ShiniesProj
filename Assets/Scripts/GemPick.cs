8d23f480889046877f4dfa4b3619e92f08a4e935
Beodrich <60353906+Beodrich@users.noreply.github.com>
2020-04-07 10:04:57 -0500

    Revert "Merge branch 'NewMasterBranch' of https://github.com/Duskitty/ShiniesProj into NewMasterBranch"
    This reverts commit bd112f4903b6b17c89148fdb8232c60610ab4f77, reversing
    changes made to 0e3cf8aca92b439e57a6464a41b10f49743a344a.

--- Assets/Scripts/GemPick.cs
+++ Assets/Scripts/GemPick.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class GemPick : MonoBehaviour
 {
-  public static bool reflectGem = false;
-  public static bool fireGem = false;
-  public static bool iceGem = false;
+  public bool reflectGem = false;
+  public bool fireGem = false;
+  public bool iceGem = false;
   //add other gems later 
   private Color fireColor;
   private Color reflectColor;
