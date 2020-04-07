abf66d39aa5734aba38eb720b144e8ab01975bc5
Beodrich <60353906+Beodrich@users.noreply.github.com>
2020-04-07 13:19:09 -0500

    stuff

--- Assets/Scripts/ChargeGemPickup.cs
+++ Assets/Scripts/ChargeGemPickup.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 
 public class ChargeGemPickup : MonoBehaviour
 {
-    public GameObject chargeGem1, gameCharge;
+    public GameObject chargeGem1, gameCharge,transition;
     private void Start()
     {
         chargeGem1.gameObject.SetActive(false);
         //GameObject.charge.GetComponent<BoxCollider2D>().enabled = false;
-        GameObject.gameCharge.GetComponet<BoxCollider2D>().enabled = false;
+        gameCharge.GetComponent<BoxCollider2D>().enabled = false;
+        transition.gameObject.GetComponent<BoxCollider2D>().enabled = false;
 
     }
     public void DeadBoss()
     {
         if (B1Script.health == 0)
         {
-            GameObject.gameCharge.GetComponet<BoxCollider2D>().enabled = true;
+            gameCharge.GetComponent<BoxCollider2D>().enabled = true;
            chargeGem1.gameObject.SetActive(true);
+            transition.gameObject.GetComponent<BoxCollider2D>().enabled = true;
            // GameObject.FindWithTag("charge").GetComponent<BoxCollider2D>().enabled = true;
         }
     }
