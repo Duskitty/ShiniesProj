5ead7829d8ddbbbe3b822dcc385dc7f2fe039414
Beodrich <60353906+Beodrich@users.noreply.github.com>
2020-04-07 17:21:02 -0500

    stuff

---
+++ Assets/Scripts/SheildBashDisapled.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheildBashDisapled : MonoBehaviour
+{
+    public GameObject bash;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //to do get rid of this just a temp fix
+        GameObject.Find("Player").GetComponent<SheildBash>().enabled = false;//to do delete me after sprint on 4-7-2020
+        bash.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
