f9bf7b0f159ac783f6b4cb3f55e2c103caa32ed8
Cole <49996960+CWoulf@users.noreply.github.com>
2020-04-14 11:08:21 -0500

    title stuff and idk

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public string StartLevel;
+    public string LevelSelect;
+
+    public void newGame()
+    {
+        Application.LoadLevel(StartLevel);
+    }
+    public void levelSelect()
+    {
+
+        Application.LoadLevel(LevelSelect);
+    }
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
