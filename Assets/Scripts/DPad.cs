c0520c34952ba65ced0e76e4a567c86d3721d5ca
Beodrich <60353906+Beodrich@users.noreply.github.com>
2020-04-30 12:43:40 -0500

    stuff

--- Assets/Scripts/DPad.cs
+++ Assets/Scripts/DPad.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class DPad : MonoBehaviour
 {
+    private bool isUsingDPad = false;
     public float speed = 3f;
     Animator animator;
     // Start is called before the first frame update
@@ -15,22 +16,30 @@ public class DPad : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetMouseButtonDown(0))
+        {
+            isUsingDPad = true;
+        }
+        else {
+            isUsingDPad = false;
+        }
     }
    public void Up()
     {
+        if (isUsingDPad)
+        {
+            transform.Translate(new Vector3(0f, speed * Time.deltaTime, 0f));
+            animator.SetBool("isUp", true);
+            animator.SetBool("isDown", false);
+            animator.SetBool("isLeft", false);
+            animator.SetBool("isRight", false);
+            animator.SetBool("isMoving", true);
 
-        transform.Translate(new Vector3(0f, speed * Time.deltaTime, 0f));
-        animator.SetBool("isUp", true);
-        animator.SetBool("isDown", false);
-        animator.SetBool("isLeft", false);
-        animator.SetBool("isRight", false);
-        animator.SetBool("isMoving", true);
-
-       PlayerMovement. isMovingUp = true;
-        PlayerMovement.isMovingDown = false;
-        PlayerMovement.isMovingLeft = false;
-        PlayerMovement.isMovingRight = false;
+            PlayerMovement.isMovingUp = true;
+            PlayerMovement.isMovingDown = false;
+            PlayerMovement.isMovingLeft = false;
+            PlayerMovement.isMovingRight = false;
+        }
     }
     public void Down()
     {
