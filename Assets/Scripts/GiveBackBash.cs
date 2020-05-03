580e46b7f24cf55c81164d576a1eedd02c96703b
Beodrich <60353906+Beodrich@users.noreply.github.com>
2020-05-03 12:06:31 -0500

    stuff

--- Assets/Scripts/GiveBackBash.cs
+++ Assets/Scripts/GiveBackBash.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class GiveBackBash : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject bashButton;
     void Start()
     {
         GameObject.Find("Player").GetComponent<SheildBash>().enabled = true;
-        GameObject.Find("BashButton").SetActive(true);
+        bashButton.SetActive(true);
     }
 
     // Update is called once per frame
