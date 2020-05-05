a25dd3c8ab4d4259e32daee35ab8f71884758700
Cole <49996960+CWoulf@users.noreply.github.com>
2020-05-05 10:38:18 -0500

    easter egg

---
+++ Assets/Textures/entities/forest/BeedleBro/Special.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Special : MonoBehaviour
+{
+   public void WTF()
+    {
+        SceneManager.LoadScene("World 3 P2");
+    }
+}
