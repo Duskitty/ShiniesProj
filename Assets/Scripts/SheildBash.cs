5a7f936a74ec579c37b6484a99dcc518e449cbbe
Beodrich <60353906+Beodrich@users.noreply.github.com>
2020-05-05 11:33:13 -0500

    stuff

--- Assets/Scripts/SheildBash.cs
+++ Assets/Scripts/SheildBash.cs
@@ -16,113 +16,126 @@ public class SheildBash : MonoBehaviour
     private bool hasPressedBar = false;//checking if space bar has been pressed bar more than once to prevent double pressing
     void Update()
     {
-        horzMov = Input.GetAxis("Horizontal");
-        verticalMov = Input.GetAxis("Vertical");
-       // Debug.Log(pickUpMirror.hasSheild);
-        if (Input.GetKey(KeyCode.Space) && pickUpMirror.hasSheild == true && hasPressedBar == false)
-        {
-            player.GetComponent<PlayerMovement>().enabled = false;//disable player input
-           // GameObject.Find("DPadController").GetComponent<SetDPad>().DisablePad();
-
-            isSheildBashing = true;
-            hasPressedBar = true;
-            PlayerDirection();
-
-
-        }
-
-
-    }
-    public void PlayerDirection()
-    {//figure out what way the player is facing then apply the right force
-        if (PlayerMovement.isMovingLeft)
-        {
-            controller.AddForce(new Vector2(-speed, 0f));
-        }
-        if (PlayerMovement.isMovingRight)
-        {
-            controller.AddForce(new Vector2(speed, 0f));
-
-
-        }
-        if (PlayerMovement.isMovingUp)
-        {
-            controller.AddForce(new Vector2(0f, speed));
-
-        }
-        if (PlayerMovement.isMovingDown)
-        {
-            controller.AddForce(new Vector2(0f, -speed));
-
-
-        }
-    }
-    private void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.CompareTag("enemy") || col.gameObject.CompareTag("bridge"))
-        {
-            //do nothing else statment is to pervent sticking to things
-
-        }
-        else if (col.gameObject.CompareTag("Cactus") || col.gameObject.CompareTag("rock") || col.gameObject.CompareTag("orb") || col.gameObject.CompareTag("sunbeam") || col.gameObject.CompareTag("mirage") || col.gameObject.CompareTag("Boss3") || col.gameObject.CompareTag("Boss") || col.gameObject.CompareTag("button") || col.gameObject.CompareTag("sign") || col.gameObject.CompareTag("Torch"))
+        if (pickUpMirror.hasSheild == true && hasPressedBar == true && GetComponent<PlayerMovement>().enabled == false)
         {
             RestoreMovment();
 
         }
-        else if (col.gameObject.name == "RockBug" || col.gameObject.CompareTag("stairs") || col.gameObject.CompareTag("break rock")) {
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+
+                RestoreMovment();
+            }
+            horzMov = Input.GetAxis("Horizontal");
+            verticalMov = Input.GetAxis("Vertical");
+            // Debug.Log(pickUpMirror.hasSheild);
+            if (Input.GetKey(KeyCode.Space) && pickUpMirror.hasSheild == true && hasPressedBar == false)
+            {
+                player.GetComponent<PlayerMovement>().enabled = false;//disable player input
+                                                                      // GameObject.Find("DPadController").GetComponent<SetDPad>().DisablePad();
+
+                isSheildBashing = true;
+                hasPressedBar = true;
+                PlayerDirection();
+
+
+            }
+
+
+        }
+    }
+        public void PlayerDirection()
+        {//figure out what way the player is facing then apply the right force
+            if (PlayerMovement.isMovingLeft)
+            {
+                controller.AddForce(new Vector2(-speed, 0f));
+            }
+            if (PlayerMovement.isMovingRight)
+            {
+                controller.AddForce(new Vector2(speed, 0f));
+
+
+            }
+            if (PlayerMovement.isMovingUp)
+            {
+                controller.AddForce(new Vector2(0f, speed));
+
+            }
+            if (PlayerMovement.isMovingDown)
+            {
+                controller.AddForce(new Vector2(0f, -speed));
+
+
+            }
+        }
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            if (col.gameObject.CompareTag("enemy") || col.gameObject.CompareTag("bridge"))
+            {
+                //do nothing else statment is to pervent sticking to things
+
+            }
+            else if (col.gameObject.CompareTag("Cactus") || col.gameObject.CompareTag("rock") || col.gameObject.CompareTag("orb") || col.gameObject.CompareTag("sunbeam") || col.gameObject.CompareTag("mirage") || col.gameObject.CompareTag("Boss3") || col.gameObject.CompareTag("Boss") || col.gameObject.CompareTag("button") || col.gameObject.CompareTag("sign") || col.gameObject.CompareTag("Torch"))
+            {
+                RestoreMovment();
+
+            }
+            else if (col.gameObject.name == "RockBug" || col.gameObject.CompareTag("stairs") || col.gameObject.CompareTag("break rock")) {
+                RestoreMovment();
+            }
+
+
+
+
+            else
+            {//for other things without a tag
+                RestoreMovment();
+
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("bridge"))
+            {
+
+                RestoreMovment();
+            }
+        }
+        public void RestoreMovment()
+        {
+            controller.velocity = Vector2.zero;//remove the forces of the sheild bash
+            player.GetComponent<PlayerMovement>().enabled = true;//enable player movment again
+            isSheildBashing = false;
+            hasPressedBar = false;
+            //  GameObject.Find("DPadController").GetComponent<SetDPad>().EnablePad();
+
+
+        }
+        public void AttackButton()
+        {
+            if (SceneManager.GetActiveScene().name == "World 2 P2")
+            {
+                return;
+
+            }
+            if (pickUpMirror.hasSheild == true && hasPressedBar == false)
+            {
+                player.GetComponent<PlayerMovement>().enabled = false;//disable player input
+                isSheildBashing = true;
+                hasPressedBar = true;
+                //  GameObject.Find("DPadController").GetComponent<SetDPad>().DisablePad();
+                PlayerDirection();
+
+
+            }
+        }
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
             RestoreMovment();
         }
-       
 
-
-            
-        else
-        {//for other things without a tag
-            RestoreMovment();
-
-        }
-    }
-   
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("bridge"))
-        {
-
-            RestoreMovment();
-        }
-    }
-    public void RestoreMovment()
-    {
-        controller.velocity = Vector2.zero;//remove the forces of the sheild bash
-        player.GetComponent<PlayerMovement>().enabled = true;//enable player movment again
-        isSheildBashing = false;
-        hasPressedBar = false;
-      //  GameObject.Find("DPadController").GetComponent<SetDPad>().EnablePad();
-
-
-    }
-    public void AttackButton()
-    {
-        if(SceneManager.GetActiveScene().name=="World 2 P2")
-        {
-            return;
-
-        }
-        if (pickUpMirror.hasSheild == true && hasPressedBar==false)
-        {
-            player.GetComponent<PlayerMovement>().enabled = false;//disable player input
-            isSheildBashing = true;
-            hasPressedBar = true;
-          //  GameObject.Find("DPadController").GetComponent<SetDPad>().DisablePad();
-            PlayerDirection();
-
-
-        }
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        RestoreMovment();
-    }
-
+    
 }
 
