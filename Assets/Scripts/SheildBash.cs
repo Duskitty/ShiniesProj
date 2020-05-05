cd15e0a8063bc0223d404eceec5bc30cd59a9062
Beodrich <60353906+Beodrich@users.noreply.github.com>
2020-05-05 14:14:32 -0500

    stuff

--- Assets/Scripts/SheildBash.cs
+++ Assets/Scripts/SheildBash.cs
@@ -71,12 +71,12 @@ public class SheildBash : MonoBehaviour
         }
         private void OnCollisionEnter2D(Collision2D col)
         {
-            if (col.gameObject.CompareTag("enemy") || col.gameObject.CompareTag("bridge"))
+            if (col.gameObject.CompareTag("enemy") || col.gameObject.CompareTag("bridge")||  col.gameObject.CompareTag("Boss")  )
             {
             //do nothing else statment is to pervent sticking to things
 
             }
-            else if (col.gameObject.CompareTag("Cactus") || col.gameObject.CompareTag("rock") || col.gameObject.CompareTag("orb") || col.gameObject.CompareTag("sunbeam") || col.gameObject.CompareTag("mirage") || col.gameObject.CompareTag("Boss3") || col.gameObject.CompareTag("Boss") || col.gameObject.CompareTag("button") || col.gameObject.CompareTag("sign") || col.gameObject.CompareTag("Torch"))
+            else if (col.gameObject.CompareTag("Cactus") || col.gameObject.CompareTag("rock") || col.gameObject.CompareTag("orb") || col.gameObject.CompareTag("sunbeam") || col.gameObject.CompareTag("mirage") ||col.gameObject.CompareTag("Boss3") || col.gameObject.CompareTag("button") || col.gameObject.CompareTag("sign") || col.gameObject.CompareTag("Torch"))
             {
                 RestoreMovment();
 
